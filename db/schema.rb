5deaa505491a7eb96c8ff3fd497560d368204252
Jiseon Sim <carol.simjs@gmail.com>
2018-07-06 11:28:48 -0700

    User Signup complete

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180706012547) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
