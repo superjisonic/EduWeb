3955a5701df7fea0d45a3af33430d462b9b28662
Jiseon Sim <carol.simjs@gmail.com>
2018-07-09 14:09:51 -0700

    Problem variable set

--- db/migrate/20180705172427_create_problems.rb
+++ db/migrate/20180705172427_create_problems.rb
@@ -1,9 +1,16 @@
 class CreateProblems < ActiveRecord::Migration[5.1]
   def change
     create_table :problems do |t|
-      t.string :title
-      t.integer :answer
       t.belongs_to :course
+      t.string :pcode
+      t.string :domain1
+      t.string :domain2
+      t.string :domain3
+      t.string :depth
+      t.integer :complexity
+      t.integer :difficulty
+      t.double :error
+      t.integer :answer
 
       t.timestamps
     end
