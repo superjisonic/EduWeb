75369ce25eef321b8756158378cbb31f05a31414
Jiseon Sim <carol.simjs@gmail.com>
2018-07-18 14:03:15 -0700

    changed...migration file

--- db/migrate/20180716180937_create_problem_records.rb
+++ db/migrate/20180716180937_create_problem_records.rb
@@ -4,7 +4,7 @@ class CreateProblemRecords < ActiveRecord::Migration[5.1]
       t.references :user
       
       t.integer :user_id, foreign_key:true
-      add_foreign_key :users, :user
+      add_foreign_key :users, :user_id
       
       t.string :pcode, foreign_key: true
       add_foreign_key :problems, :pcode
